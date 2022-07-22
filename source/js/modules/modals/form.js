09ea50fcfa62ed599c2d3f76d79816c1d80fd7be
Наталья Салабай <salabay.natalia89@gmail.com>
2022-07-23 01:48:54 +0300

    Шрифты, графика.

---
+++ source/js/modules/modals/form.js
@@ -0,0 +1,16 @@
+// const inputs = document.querySelectorAll('input[data-rule]');
+
+// for (const input of inputs) {
+//   input.addEventListener('blur', function () {
+//     const rule = this.dataset.rule;
+//     const value = this.value;
+
+//     switch (rule) {
+//       case 'number':
+
+//       break;
+//       case 'email':
+
+//     }
+//   });
+// }
