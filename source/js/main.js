2340016d42ad713900ce647e302b4b33b67291a8
unknown <salabay.natalia89@gmail.com>
2022-09-03 15:11:21 +0300

    правки

--- source/js/main.js
+++ source/js/main.js
@@ -1,22 +1,10 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
 import {menu} from './modules/menu';
 import {anchor} from './modules/anchor';
 
-// ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
-
-  // Utils
-  // ---------------------------------
-
   iosVhFix();
-
-  // Modules
-  // ---------------------------------
-
-  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-  // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
     menu();
     anchor();
